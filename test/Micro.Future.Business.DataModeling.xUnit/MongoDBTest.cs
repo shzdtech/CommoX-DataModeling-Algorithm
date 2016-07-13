28ac47107a398788a54d947bc2396ce534c44c25
myuss <myuss@DESKTOP-3891J1L>
2016-07-13 16:50:41 +0800

    Add xUnit support

---
+++ test/Micro.Future.Business.DataModeling.xUnit/MongoDBTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Micro.Future.Business.DataModeling.xUnit
+{
+    public class MongoDBTest
+    {
+        public MongoDBTest()
+        {
+        }
+
+        [Fact]
+        public void TestConnection()
+        {
+            
+        }
+    }
+}

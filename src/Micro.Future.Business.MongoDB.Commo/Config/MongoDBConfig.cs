d804a3cd35045f633b71d1970664c14dad6891a1
titicaca <fangzhou.sjtu@gmail.com>
2016-07-24 21:59:44 +0800

    add mongo interface for requirement

---
+++ src/Micro.Future.Business.MongoDB.Commo/Config/MongoDBConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Micro.Future.Business.MongoDB.Commo.Config
+{
+    public static class MongoDBConfig
+    {
+        public const String mongoAddr = "mongodb://root:shzl2701@114.55.54.144:3717";
+        public const String COLLECTION_REQUIREMENT = "requirement";
+        public const String COLLECTION_CHAIN = "chain";
+        public const String DATABASE = "testdb";
+    }
+}
---
+++ src/Micro.Future.Business.MongoDB.Commo/MongoInterface/IRequirement.cs
@@ -0,0 +1,24 @@
+﻿using Micro.Future.Business.MongoDB.Commo.BizObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Micro.Future.Business.MongoDB.Commo.MongoInterface
+{
+    public interface IRequirement
+    {
+        void AddRequirement(RequirementObject requirement);
+
+        bool CancelRequirement(int requirementId);
+
+        IEnumerable<RequirementObject> QueryRequirements(string userId);
+
+        RequirementObject QueryRequirementInfo(int requirementId);
+
+        IEnumerable<ChainObject> QueryRequirementChains(int requirementId);
+    }
+
+
+
+}

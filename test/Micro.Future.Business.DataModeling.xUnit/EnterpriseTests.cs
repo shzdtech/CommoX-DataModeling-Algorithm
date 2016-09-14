f34a8990ea17018047542f48c6afacfb24b9563c
ymgan <ymgan@ctrip.com>
2016-09-14 12:05:59 +0800

    dataaccess unit testing in memory

---
+++ test/Micro.Future.Business.DataModeling.xUnit/EnterpriseTests.cs
@@ -0,0 +1,52 @@
+﻿using Micro.Future.Business.DataAccess.Commo;
+using Micro.Future.Business.DataAccess.Commo.CommoHandler;
+using Micro.Future.Business.DataAccess.Commo.CommonInterface;
+using Micro.Future.Business.DataAccess.Commo.CommoObject;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Micro.Future.Business.DataModeling.xUnit
+{
+    public class EnterpriseTests
+    {
+        private CommoXContext _context = null;
+        public EnterpriseTests()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<CommoXContext>();
+            optionsBuilder.UseInMemoryDatabase();
+
+            _context = new CommoXContext(optionsBuilder.Options);
+        }
+
+
+        [Fact]
+        public void Test_AddEnterprise()
+        {
+            IEnterprise service = new EnterpriseHandler(_context);
+
+            Enterprise enterprise = new Enterprise()
+            {
+                Name = "test company",
+                Address = "test address",
+                EmailAddress = "test@test.com",
+                Contacts = "test",
+                MobilePhone = "12345678901",
+                Fax = "021123456"
+            };
+
+            var newEnterprise = service.AddEnterprise(enterprise);
+            
+
+            Assert.NotEqual<int>(0, newEnterprise.EnterpriseId);
+            Assert.Equal<string>(enterprise.Name, newEnterprise.Name);
+
+        }
+    }
+}
